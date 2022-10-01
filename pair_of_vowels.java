b7a9c66fd548f995080dc778546428bc50f9dc3b
Alex Programmer <63038222+alexprogrammer07@users.noreply.github.com>
2022-10-01 15:02:46 +0530

    Initial Commit

---
+++ pair_of_vowels.java
@@ -0,0 +1,33 @@
+package com.akshat.string_programs;
+
+//Doubt : Why Input with space gives wrong output. Input without space gives right output.
+
+import java.util.Scanner;
+
+public class pair_of_vowels {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the String :");
+        String str = sc.next();
+        String strnew="";
+        int len=str.length(),i;
+        char ch,ch1;
+
+        for (i=1;i<=len-1;i++)
+        {
+            ch=str.charAt(i-1);
+            ch1=str.charAt(i);
+
+            if (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
+            {
+                if (ch1=='a' || ch1=='e' || ch1=='i' || ch1=='o' || ch1=='u')
+                {
+                    strnew = strnew + ch+ch1+"   ";
+                }
+            }
+        }
+
+        System.out.println("Pair of vowels in a String are : "+strnew);
+    }
+}
